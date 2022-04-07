2601344c848ff86fc18eab40f35012c75ec1520b
aersune <aersune@gmail.com>
2022-04-07 10:39:40 +0500

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:restaurant_recipe/pages/home_page.dart';
 
 
-void main() {
-  runApp(const MyApp());
-}
+void main() =>  runApp(const MyApp());
+
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
